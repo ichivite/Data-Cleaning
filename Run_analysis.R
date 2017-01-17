0ca9e66357a7442cf05e2d88c7141c34957ab5b2
ichivite <chivite@hotmail.com>
2017-01-17 19:26:41 +0100

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,69 +1,69 @@
-library(plyr)
+# libraries needed
+library(dplyr)
 
-# Step 1
-# Merge the training and test sets to create one data set
-###############################################################################
+#name of the new folder
+folder <- "./get_and_cleaning_data_week_4"
 
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+#check and create folder
 
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+if(!file.exists(folder)){
+  dir.create(folder)}
 
-# create 'x' data set
-x_data <- rbind(x_train, x_test)
+#download data
 
-# create 'y' data set
-y_data <- rbind(y_train, y_test)
+data.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-# create 'subject' data set
-subject_data <- rbind(subject_train, subject_test)
+download.file(data.url,destfile="./get_and_cleaning_data_week_4/data.zip",method="auto")
 
-# Step 2
-# Extract only the measurements on the mean and standard deviation for each measurement
-###############################################################################
+unzip(zipfile="./get_and_cleaning_data_week_4/data.zip",exdir="./get_and_cleaning_data_week_4")
 
-features <- read.table("features.txt")
+#Set new working directory, make sit easier to work
+setwd("C:/Users/IÑIGO/Documents/coursera/get_and_cleaning_data_week_4/UCI HAR Dataset")
 
-# get only columns with mean() or std() in their names
-mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+#########################
+##1. Merges the training and the test sets to create one data set.
+############################
 
-# subset the desired columns
-x_data <- x_data[, mean_and_std_features]
 
-# correct the column names
-names(x_data) <- features[mean_and_std_features, 2]
+#I start reading tables:
 
-# Step 3
-# Use descriptive activity names to name the activities in the data set
-###############################################################################
+#train files:
+x_train <- read.table("./train/X_train.txt")
+y_train <- read.table("./train/y_train.txt")
+subject_train <- read.table("./train/subject_train.txt")
+ 
+#test files
+x_test <- read.table("./test/X_test.txt")
+y_test <- read.table("./test/y_test.txt")
+subject_test <- read.table("./test/subject_test.txt")
 
-activities <- read.table("activity_labels.txt")
 
-# update values with correct activity names
-y_data[, 1] <- activities[y_data[, 1], 2]
 
-# correct column name
-names(y_data) <- "activity"
 
-# Step 4
-# Appropriately label the data set with descriptive variable names
-###############################################################################
 
-# correct column name
-names(subject_data) <- "subject"
 
-# bind all the data in a single data set
-all_data <- cbind(x_data, y_data, subject_data)
+#########################
+##2. Extracts only the measurements on the mean and standard deviation for each measurement.
+############################
 
-# Step 5
-# Create a second, independent tidy data set with the average of each variable
-# for each activity and each subject
-###############################################################################
 
-# 66 <- 68 columns but last two (activity & subject)
-averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+#########################
+##3. Uses descriptive activity names to name the activities in the data set
+############################
 
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
+
+
+
+
+
+#########################
+##4. Appropriately labels the data set with descriptive variable names.
+############################
+
+
+
+#########################
+##5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+############################
+
+
